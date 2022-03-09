d847573ea8aa76afe39ce3b99f9b583f9764dddf
HAOUARI Noureddine <haouarin@hotmail.com>
2022-03-09 08:22:51 +0100

    v0.0.4 refactoring

--- main.ts
+++ main.ts
@@ -1,34 +1,33 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { exit } from 'process';
+import {App, Notice, Plugin, PluginSettingTab, Setting, request, MarkdownView, Editor} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface TextGeneratorSettings {
-    api_key: string;
-	engine: string; 
-	max_tokens: number; 
+	api_key: string;
+	engine: string;
+	max_tokens: number;
 	temperature: number;
 	frequency_penalty: number;
-	prompt:string;
-	showStatusBar:boolean;
+	prompt: string;
+	showStatusBar: boolean;
 }
 
 const DEFAULT_SETTINGS: TextGeneratorSettings = {
 	api_key: "",
-    engine: "text-davinci-001",
+	engine: "text-davinci-001",
 	max_tokens: 160,
 	temperature: 0.7,
 	frequency_penalty: 0.5,
-	prompt:"",
-	showStatusBar:true
+	prompt: "",
+	showStatusBar: true
 }
 
 
 export default class TextGeneratorPlugin extends Plugin {
 	settings: TextGeneratorSettings;
-	statusBarItemEl:any;
+	statusBarItemEl: any;
 
-    async getGeneratedText(params:TextGeneratorSettings) {
+	async getGeneratedText(params: TextGeneratorSettings) {
 
 		const usedParams = {
 			"prompt": params.prompt,
@@ -37,81 +36,100 @@ export default class TextGeneratorPlugin extends Plugin {
 			"frequency_penalty": params.frequency_penalty,
 		};
 
-        const reqParams = {
-            url: `https://api.openai.com/v1/engines/${params.engine}/completions`,
-            method: 'POST',
-            body: JSON.stringify(usedParams),
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${params.api_key}`
-            },
-        }
-        const requestResults = JSON.parse(await window.request(reqParams));
-        const text=requestResults.choices[0].text
-        return text
-    }
+		const reqParams = {
+			url: `https://api.openai.com/v1/engines/${params.engine}/completions`,
+			method: 'POST',
+			body: JSON.stringify(usedParams),
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${params.api_key}`
+			},
+		}
+		const requestResults = JSON.parse(await request(reqParams));
+		const text = requestResults.choices[0].text
+		return text
+	}
 
-	 getMetaData() {
-		 const cache=this.app.metadataCache.getCache(app.workspace.getActiveFile().path);
-		 let metadata = ""
-		 if(cache.hasOwnProperty('frontmatter')){
-			const frontmatter = cache.frontmatter;
-			delete frontmatter.position; 
+	getMetaData() {
+		const activeFile = this.app.workspace.getActiveFile();
+		let metadata = ""
+		if (activeFile !== null) {
+			const cache = this.app.metadataCache.getCache(activeFile.path);
+			if (cache.hasOwnProperty('frontmatter')) {
+				const frontmatter = cache.frontmatter;
 
-			for(const [key, value] of Object.entries(frontmatter)) {
-				if(Array.isArray(value)) {
-					metadata+=`${key} : `
-					value.forEach(v=>{
-						metadata+=`${value}, `
-					})
-					metadata+=`\n`
-				} else {
-					metadata+=`${key} : ${value} \n`
+				for (const [key, value] of Object.entries(frontmatter)) {
+					if (key === 'position') continue;
+
+					if (Array.isArray(value)) {
+						metadata += `${key} : `
+						value.forEach(v => {
+							metadata += `${value}, `
+						})
+						metadata += `\n`
+					} else {
+						metadata += `${key} : ${value} \n`
+					}
 				}
 			}
 		}
 		return metadata;
-	 }
-
-     insertGeneratedText(text:string) {
-        const cursor=this.app.workspace.activeLeaf.view.currentMode.editor.getCursor();
-		this.app.workspace.activeLeaf.view.currentMode.editor.replaceRange(text,cursor)
-      }
-
-    async complete(params:TextGeneratorSettings,insertMetadata:boolean=false){
-    	if (insertMetadata) {
-			const metadata = this.getMetaData();
-			if(metadata.length ===0) {
-			new Notice("No frontmatter, or it is not well formated!");
-			return;
-			} else 
-			{
-				params.prompt=  this.getMetaData() +params.prompt; 
-			}
+	}
+	getActiveView() {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView !== null) {
+			return activeView
+		} else {
+			new Notice("The file type should be Markdown!");
+			return null
 		}
-		
-		console.log(params)
-		let text = await this.getGeneratedText(params);
-        this.insertGeneratedText(text)
-    }	
-
-	getPrompt() {
-		this.app.workspace.activeLeaf.view.editor.getLine(this.app.workspace.activeLeaf.view.editor.getCursor().line)
-		const leaf = this.app.workspace.activeLeaf;
-		let selectedText= leaf.view.currentMode.getSelection();
-		if(selectedText.length===0) {
-			selectedText=this.app.workspace.activeLeaf.view.editor.getLine(this.app.workspace.activeLeaf.view.editor.getCursor().line);
-		}
-		return selectedText;
 	}
 
-	updateStatusBar(text:string){
-		let text2=""
-		if(text.length>0) {
-			const text2=`: ${text}`
+	insertGeneratedText(text: string) {
+		const activeView = this.getActiveView();
+		if (activeView !== null) {
+			const cursor = activeView.editor.getCursor();
+			activeView.editor.replaceRange(text, cursor);
 		}
-	
-		if(this.settings.showStatusBar) {
+	}
+
+	async complete(params: TextGeneratorSettings, insertMetadata: boolean = false) {
+		if (insertMetadata) {
+			const metadata = this.getMetaData();
+			if (metadata.length === 0) {
+				new Notice("Please provide a valid frontmatter!");
+				return;
+			} else {
+				params.prompt = this.getMetaData() + params.prompt;
+			}
+		}
+
+		console.log(params)
+		let text = await this.getGeneratedText(params);
+		this.insertGeneratedText(text)
+	}
+
+	getPrompt() {
+		const activeView = this.getActiveView();
+		if (activeView !== null) {
+			let selectedText = activeView.editor.getSelection();
+
+			if (selectedText.length === 0) {
+				const lineNumber = activeView.editor.getCursor().line;
+				selectedText = activeView.editor.getLine(lineNumber);
+			}
+			return selectedText;
+		}
+
+	}
+
+	updateStatusBar(text: string) {
+		let text2 = ""
+		if (text.length > 0) {
+			const text2 = `: ${text}`
+		}
+
+		if (this.settings.showStatusBar) {
 			this.statusBarItemEl.setText(`Text Generator(${this.settings.max_tokens})${text2}`)
 		}
 	}
@@ -123,42 +141,37 @@ export default class TextGeneratorPlugin extends Plugin {
 		const ribbonIconEl = this.addRibbonIcon('pencil', 'Text Generator', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			this.updateStatusBar(`processing... `);
-            await this.complete({...this.settings,prompt:this.getPrompt()});
+			await this.complete({
+				...this.settings,
+				prompt: this.getPrompt()
+			});
 			this.updateStatusBar(``);
 		});
-		
+
 		this.addCommand({
 			id: 'generate-text',
 			name: 'Generate Text!',
-			checkCallback: async (checking: boolean) => {
-				const leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-                        this.updateStatusBar(`processing... `);
-						await this.complete({...this.settings,prompt:this.getPrompt()});
-                        this.updateStatusBar(``);
-					}
-					return true;
-				}
-				return false;
+			editorCallback: async (editor: Editor) => {
+				this.updateStatusBar(`processing... `);
+				await this.complete({
+					...this.settings,
+					prompt: this.getPrompt()
+				});
+				this.updateStatusBar(``);
 			}
 		});
 
 		this.addCommand({
 			id: 'generate-text-with-metadata',
 			name: 'Generate Text (use Metadata))!',
-			checkCallback: async (checking: boolean) => {
-				const leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						this.updateStatusBar(`processing... `);
-						const settings = {...this.settings, prompt:this.getPrompt()};
-                        await this.complete(settings,true);
-                        this.updateStatusBar(``);
-					}
-					return true;
-				}
-				return false;
+			editorCallback: async (editor: Editor) => {
+				this.updateStatusBar(`processing... `);
+				const settings = {
+					...this.settings,
+					prompt: this.getPrompt()
+				};
+				await this.complete(settings, true);
+				this.updateStatusBar(``);
 			}
 		});
 
@@ -166,40 +179,26 @@ export default class TextGeneratorPlugin extends Plugin {
 		this.addCommand({
 			id: 'increase-max_tokens',
 			name: 'Increase max_tokens by 10',
-			checkCallback: async (checking: boolean) => {
-				const leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						this.settings.max_tokens += 10 ;
-						await this.saveSettings();
-                        this.updateStatusBar('');
-					}
-					return true;
-				}
-				return false;
+			editorCallback: async (editor: Editor) => {
+				this.settings.max_tokens += 10;
+				await this.saveSettings();
+				this.updateStatusBar('');
 			}
 		});
 
 		this.addCommand({
 			id: 'decrease-max_tokens',
 			name: 'decrease max_tokens by 10',
-			checkCallback: async (checking: boolean) => {
-				const leaf= this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						this.settings.max_tokens -= 10 ;
-						await this.saveSettings();
-						this.updateStatusBar('');
-					}
-					return true;
-				}
-				return false;
+			editorCallback: async (editor: Editor) => {
+				this.settings.max_tokens -= 10;
+				await this.saveSettings();
+				this.updateStatusBar('');
 			}
 		});
 
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new TextGeneratorSettingTab(this.app, this));
 
 	}
 
@@ -217,7 +216,7 @@ export default class TextGeneratorPlugin extends Plugin {
 }
 
 
-class SampleSettingTab extends PluginSettingTab {
+class TextGeneratorSettingTab extends PluginSettingTab {
 	plugin: TextGeneratorPlugin;
 
 	constructor(app: App, plugin: TextGeneratorPlugin) {
@@ -226,11 +225,15 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const {
+			containerEl
+		} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for OpenAI.'});
+		containerEl.createEl('h2', {
+			text: 'Settings for OpenAI.'
+		});
 
 		new Setting(containerEl)
 			.setName('api_key')
@@ -243,10 +246,10 @@ class SampleSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 
-        new Setting(containerEl)
-        .setName('engine')
-        .setDesc('engine')
-		.addDropdown((cb) => {
+		new Setting(containerEl)
+			.setName('engine')
+			.setDesc('engine')
+			.addDropdown((cb) => {
 				cb.addOption("text-davinci-001", "text-davinci-001");
 				cb.addOption("text-curie-001", "text-curie-001");
 				cb.addOption("text-babbage-001", "text-babbage-001");
@@ -258,46 +261,46 @@ class SampleSettingTab extends PluginSettingTab {
 				});
 			})
 		new Setting(containerEl)
-		.setName('max_tokens')
-		.setDesc('max_tokens')
-		.addText(text => text
-			.setPlaceholder('max_tokens')
-			.setValue(this.plugin.settings.max_tokens.toString())
-			.onChange(async (value) => {
-				this.plugin.settings.max_tokens = parseInt(value) ;
-				await this.plugin.saveSettings();
-			}));
-
-        new Setting(containerEl)
-        .setName('temperature')
-        .setDesc('temperature')
-        .addText(text => text
-            .setPlaceholder('temperature')
-            .setValue(this.plugin.settings.temperature.toString())
-            .onChange(async (value) => {
-                this.plugin.settings.temperature = parseFloat(value) ;
-                await this.plugin.saveSettings();
-            }));
+			.setName('max_tokens')
+			.setDesc('max_tokens')
+			.addText(text => text
+				.setPlaceholder('max_tokens')
+				.setValue(this.plugin.settings.max_tokens.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.max_tokens = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
 
 		new Setting(containerEl)
-		.setName('frequency_penalty')
-		.setDesc('frequency_penalty')
-		.addText(text => text
-			.setPlaceholder('frequency_penalty')
-			.setValue(this.plugin.settings.frequency_penalty.toString())
-			.onChange(async (value) => {
-				this.plugin.settings.frequency_penalty = parseFloat(value) ;
-				await this.plugin.saveSettings();
-			}));
+			.setName('temperature')
+			.setDesc('temperature')
+			.addText(text => text
+				.setPlaceholder('temperature')
+				.setValue(this.plugin.settings.temperature.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.temperature = parseFloat(value);
+					await this.plugin.saveSettings();
+				}));
 
 		new Setting(containerEl)
-		.setName('showStatusBar')
-		.setDesc('Show information in the Status Bar')
-		.addToggle(v=> v
-			.setValue(this.plugin.settings.showStatusBar)
-			.onChange(async (value) => {
-				this.plugin.settings.showStatusBar = value ;
-				await this.plugin.saveSettings();
-			}));
+			.setName('frequency_penalty')
+			.setDesc('frequency_penalty')
+			.addText(text => text
+				.setPlaceholder('frequency_penalty')
+				.setValue(this.plugin.settings.frequency_penalty.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.frequency_penalty = parseFloat(value);
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('showStatusBar')
+			.setDesc('Show information in the Status Bar')
+			.addToggle(v => v
+				.setValue(this.plugin.settings.showStatusBar)
+				.onChange(async (value) => {
+					this.plugin.settings.showStatusBar = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
