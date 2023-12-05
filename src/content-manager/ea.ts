a0adf17d79bbf225d41e9b16b34f0fdb30c1c780
haouarihk <haitam2001@gmail.com>
2023-12-05 19:43:49 +0100

    improving excalidraw writing

--- src/content-manager/ea.ts
+++ src/content-manager/ea.ts
@@ -12,7 +12,12 @@ export default class ExcalidrawManager implements ContentManager {
     }
 
     protected async getTextSelectedItems(): Promise<Item[]> {
-        return this.ea.getViewSelectedElements().sort((a: any, b: any) => a.y - b.y).filter((e: Item) => !e.isDeleted && !!e.rawText);
+        return this.ea.getViewSelectedElements().sort((a: any, b: any) => a.y + a.height - b.y + b.height).filter((e: Item) => !e.isDeleted && !!e.rawText);
+    }
+
+    protected getElement(id: string): Item {
+        const items = [...this.ea.getViewElements(), ...this.ea.getElements()];
+        return items.find((e: Item) => e.id == id);
     }
 
     async getSelections(): Promise<string[]> {
@@ -32,7 +37,13 @@ export default class ExcalidrawManager implements ContentManager {
     }
 
     selectTgSelection(tgSelectionLimiter?: string) {
-        return;
+        let txt = this.ea.getViewSelectedElements().map((e: Item) => e.rawText).filter(Boolean).join("\n").trim();
+
+        if (!txt?.length) {
+            txt = this.ea.getValue();
+        }
+
+        return txt;
     }
 
     getLastLetterBeforeCursor(): string {
@@ -45,8 +56,16 @@ export default class ExcalidrawManager implements ContentManager {
         return items[dir == "from" ? 0 : items.length - 1];
     }
 
-    setCursor(pos: Item): void {
-        // this.ea.viewZoomToElements([pos], [pos])
+    setCursor(pos: Item | Item[]): void {
+        let arr = [];
+
+        if (pos)
+            if (Array.isArray(pos))
+                arr = pos;
+            else
+                arr.push(pos);
+
+        this.ea.selectElementsInView(arr)
         return;
     }
 
@@ -54,8 +73,6 @@ export default class ExcalidrawManager implements ContentManager {
         const items = await this.getTextSelectedItems();
         let selectedItem = pos || this.getCursor();
         let itemId = selectedItem?.id;
-
-
 
         switch (mode) {
             case "replace":
@@ -72,52 +89,93 @@ export default class ExcalidrawManager implements ContentManager {
                 break;
         }
 
-        let item = this.ea.getElement(itemId) || selectedItem;
+        let item = this.getElement(itemId) || selectedItem;
+
+        if (pos) {
+            if (pos.strokeColor)
+                this.ea.style.strokeColor = pos.strokeColor;
+            if (pos.fontSize)
+                this.ea.style.fontSize = pos.fontSize;
+            if (pos.fontFamily)
+                this.ea.style.fontFamily = pos.fontFamily;
+        }
 
         if (!item) {
-            console.log("called create new item", { itemId, selectedItem, item });
-
-            if (pos) {
-                this.ea.style.strokeColor = pos.strokeColor;
-                this.ea.style.fontSize = pos.fontSize;
-                this.ea.style.fontFamily = pos.fontFamily;
-            }
-
             const textSize: { width: number, height: number } = this.ea.measureText(text);
 
             itemId = this.ea.addText(
-                item?.x - textSize.width / 2 - 2,
-                item?.y - textSize.height,
+                pos.x,
+                pos.y,
                 text,
                 {
                     wrapAt: 5,
-                    ...item,
+                    ...pos,
                     id: undefined,
                     box: pos?.type
-                        ? { width: Math.min(textSize.width, pos.width * 2), boxPadding: 0 }
+                        ? {
+                            width: Math.min(textSize.width + 2, Math.max((this.ea.style.fontSize) * 20, 200)),
+                            boxPadding: 0
+                        }
                         : { boxPadding: 2 }
                 }
             );
-            item = this.ea.getElement(itemId);
-            if (pos) {
-                item.x = pos.x;
-                item.y = pos.y + pos.height;
-            }
+
+
+            await this.ea.addElementsToView(false, false);
+            // this.ea.clear();
+
+            item = this.getElement(itemId);
+
+            // console.log(item, this.ea.refreshTextElementSize(itemId))
+
+            // await this.ea.addElementsToView(false, false);
+            // this.ea.clear();
         }
 
-        const elements = [item];
-        const textSize: { width: number, height: number } = this.ea.measureText(text);
+        if (item) {
+            if (item.strokeColor)
+                this.ea.style.strokeColor = item.strokeColor;
+            if (item.fontSize)
+                this.ea.style.fontSize = item.fontSize;
+            if (item.fontFamily)
+                this.ea.style.fontFamily = item.fontFamily;
+        }
+
+
+        let elements = [item];
+
+        let textSize: { width: number, height: number }
+            = this.ea.measureText((elements[0]?.text || "") + text);
 
         elements.forEach((el) => {
-            el.text = el.rawText = el?.text + text;
-            if (pos)
-                el.width = Math.min(textSize.width, pos.fontSize * 16);
-            el.height = textSize.height;
+            el.text = el.rawText = (el?.text || "") + text;
+            if (pos) {
+                el.x = pos.x;
+                el.width = Math.min(textSize.width + 10, Math.max((this.ea.style.fontSize) * 20, 200));
+                el.height = textSize.height;
+            } else {
+                el.width = textSize.width;
+            }
         });
 
-        this.ea.copyViewElementsToEAforEditing(elements);
-        this.ea.addElementsToView(false, false);
+        await this.ea.addElementsToView(false, false);
+        await this.ea.targetView?.forceSave(true);
+        this.ea.clear();
 
+        elements[0] = this.getElement(itemId);
+
+        textSize = this.ea.measureText((elements[0]?.text || "") + text);
+
+        elements.forEach((el) => {
+            if (pos)
+                el.y = (pos.y + pos.height + textSize.height) || el.y;
+        });
+
+        elements[0] = this.getElement(itemId);
+
+        await this.ea.copyViewElementsToEAforEditing(elements);
+        await this.ea.addElementsToView(false, false);
+        await this.ea.targetView?.forceSave(true);
         this.ea.clear();
 
         return item;
@@ -129,7 +187,13 @@ export default class ExcalidrawManager implements ContentManager {
         replaceAllWith(newData: string): void
     }> {
         const items = await this.getTextSelectedItems();
-        let selectedItem = items[items.length - 1]
+        let selectedItem = (
+            items.length ?
+                items
+                : this.ea.getViewElements().map((e: Item) => e.rawText).filter(Boolean)
+        )[items.length - 1]
+
+        // if (!selectedItem) throw "no selected items";
 
         // create a new item
         const startingCursor = pos;
@@ -139,7 +203,36 @@ export default class ExcalidrawManager implements ContentManager {
 
         let postingContent = "";
         let stillPlaying = true;
-        let firstTime = true;
+
+        // const testData = [
+        //     `Excalidraw is known for its hand-drawn feel and ease of use when creating diagrams or sketches that can be shared digitally or included in various documents.`,
+        //     `If you need assistance with something specific regarding an Excalidraw file, such as understanding how to open it, share it, or if you have questions about using the tool itself, feel free to ask!`,
+        //     `And then you ended with "hello world," which is often used as a test phrase in computing—one of the simplest programs one can write when learning programming outputs this message to ensure everything's set up correctly.d`,
+        //     `test`,
+        //     `If this was not what you intended to inquire about though and I've misunderstood your query somehow, please provide some more details so I can assist appropriately!`
+        // ]
+
+        // let lastPos = selectedItem;
+        // let arr = [];
+        // for (const section of testData) {
+        //     const item = await this.insertText(section, lastPos, mode)
+        //     await new Promise((s) => setTimeout(s, 500));
+        //     lastPos = item;
+        //     arr.push(item)
+        // }
+
+        // await new Promise((s) => setTimeout(s, 500));
+
+
+        // this.ea.deleteViewElements(arr)
+
+        // this.ea.addElementsToView(false, false);
+
+        // await this.ea.targetView?.forceSave(true);
+
+        // throw "testing";
+
+
 
         // const writerTimer: any = setInterval(() => {
         //     if (!stillPlaying) return clearInterval(writerTimer);
@@ -170,9 +263,21 @@ export default class ExcalidrawManager implements ContentManager {
             },
 
             replaceAllWith: async (allText) => {
-                const item = await this.insertText(allText, pos, mode)
-                this.setCursor(item);
-                // this.insertText(allText, startingCursor, "replace");
+                const sections = allText.split("\n").filter(t => t.trim());
+                let lastPos = pos || this.getCursor("to");
+                let arr: Item[] = []
+                for (const section of sections) {
+                    const item = await this.insertText(section, lastPos, mode)
+
+                    if (item) {
+                        lastPos = item;
+                    }
+
+                    await new Promise((s) => setTimeout(s, 300))
+
+                    // this.insertText(allText, startingCursor, "replace");
+                }
+                this.setCursor(arr);
             }
         }
     }
