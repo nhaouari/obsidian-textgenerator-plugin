ce7469de9ddd79c69d94367ccc4902b28b91775a
haouarihk <haitam2001@gmail.com>
2023-12-22 10:10:35 +0100

    adding uischema and formData to custom form

--- src/ui/template-input-modal/view.tsx
+++ src/ui/template-input-modal/view.tsx
@@ -14,7 +14,7 @@ if (Theme.templates) {
 
 
 if (Theme.widgets) {
-  Theme.widgets.text = function Field(props) {
+  Theme.widgets.textarea = function Field(props) {
     return <div className="flex flex-col gap-1">
       <label className="mb-2 font-bold text-gray-700">{props.label}</label>
       <textarea
@@ -47,17 +47,27 @@ export default function TemplateInputModalView(props: {
     props.p.close?.();
   };
 
-  const [schema, setSchema] = useState<FormProps["schema"]>({})
+  const [JSONSchema, setJSONSchema] = useState<FormProps["schema"]>({})
+  const [UISchema, setUISchema] = useState<FormProps["uiSchema"]>({})
+  const [formData, setFormData] = useState<FormProps["formData"]>({})
 
 
   useEffect(() => {
     (async () => {
       const basicProps: Record<string, FormProps["schema"]> = {};
+      const basicUi: Record<string, FormProps["uiSchema"]> = {};
       const required: string[] = [];
 
       props.labels.forEach(l => {
         basicProps[l] = {
           type: 'string',
+          title: l
+        }
+        basicUi[l] = {
+          "ui:widget": "textarea",
+          props: {
+            className: "w-full"
+          }
         }
         if (props.templateContext.strict && !l.contains("_optional")) required.push(l)
       })
@@ -69,19 +79,33 @@ export default function TemplateInputModalView(props: {
         required
       } as FormProps["schema"];
 
-      setSchema(obj);
+      setJSONSchema(obj);
+      setUISchema(basicUi);
 
       if (props.templateContext.templatePath) {
         const cschema = await props.p.plugin.textGenerator.contextManager.getTemplateCustomInputConfig(props.templateContext.templatePath)
-        if (cschema)
-          setSchema(cschema);
+        if (cschema) {
+          setJSONSchema({
+            ...obj,
+            ...cschema
+          });
+
+          if (cschema.uiSchema)
+            setUISchema(cschema.uiSchema);
+
+          if (cschema.formData)
+            setFormData(cschema.formData);
+        }
       }
     })()
   }, [])
 
   return (
     <Form
-      schema={schema}
+      className="w-full"
+      schema={JSONSchema}
+      uiSchema={UISchema}
+      formData={formData}
       validator={validator}
       onSubmit={handleSubmit}
     >{props.children}</Form>
