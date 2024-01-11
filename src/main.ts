f41609242d175f67ad9640453e06ac81b7b4238f
haouarihk <haitam2001@gmail.com>
2024-01-11 15:38:34 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ export default class TextGeneratorPlugin extends Plugin {
   packageManager: PackageManager;
   versionManager: VersionManager;
   contextManager: ContextManager;
-  contentManager: ContentManagerCls = ContentManagerCls;
+  contentManager: typeof ContentManagerCls = ContentManagerCls;
   tokensScope: TokensScope;
   processing: boolean;
   defaultSettings: TextGeneratorSettings;
