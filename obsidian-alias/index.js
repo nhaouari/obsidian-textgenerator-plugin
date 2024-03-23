891c3bdf5fec5d1cd6f098cb70f49b7bf4ef9bbf
haouarihk <haithem2001@gmail.com>
2024-03-23 19:03:00 +0100

    .

--- obsidian-alias/index.js
+++ obsidian-alias/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = function ObsidianAliasPlugin() {
     return alias({
         'fs': path.resolve(__dirname, `./fs.js`),
-        // 'fs-extra': path.resolve(__dirname, `./fs.js`),
+        'fs-extra': path.resolve(__dirname, `./fs.js`),
         'fs/promises': path.resolve(__dirname, `./fsPromises.js`),
         'path': path.resolve(__dirname, `./path.js`),
         'node-fetch': path.resolve(__dirname, `./node-fetch.js`),
